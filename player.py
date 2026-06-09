4e2fb7900c6b2e621fdc0fdb3ae3c465131e132a
archna-v <archnav.22cse@kongu.edu>
2026-06-09 18:59:00 +0530

    Fix formatting of symbol assignment in Player class

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 class Player:
     def __init__(self, name=None, symbol=None):
         self.name = name
-        self.symbol = symbol
+        self.symbol =symbol
 
     def set_player_name_and_symbol(self, name, symbol):
         self.name = name
